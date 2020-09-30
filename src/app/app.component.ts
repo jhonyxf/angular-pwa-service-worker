1548fca4c337badd9c0cb0fa1aebffbfbd06a32d
sidney_silva_verity <sidney.silva@verity.com.br>
2020-09-30 13:45:14 -0300

    1x up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  
   title = 'angular-pwa-service-worker';
+  apiData: any;
+  constructor(private http: HttpClient){}
+
+  ngOnInit() {
+    this.http.get('http://dummy.restapiexample.com/api/v1/employees').subscribe(
+      (res: any) => {
+        this.apiData = res.data;
+        console.log(res.data);
+      }, err => {
+        console.log(err);
+      }
+    )
+  }
+
+
 }
